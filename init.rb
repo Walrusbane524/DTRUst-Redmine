b4f4bbe58eec2241d84dc78788f0679437bebce0
Guilherme <walrusbane524@gmail.com>
2023-04-20 14:19:15 -0300

    Module

--- init.rb
+++ init.rb
@@ -5,6 +5,11 @@ Redmine::Plugin.register :tds do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
-  permission :tds, { tds: [:index, :switch] }, public: true
+  #permission :tds, { tds: [:index, :switch] }, public: true
   menu :project_menu, :tds, { controller: 'tds', action: 'index' }, caption: 'TDs', after: :activity, param: :project_id
+  
+  project_module :tds do
+    permission :view_tds, tds: :index
+    permission :create_tds, tds: :create
+  end
 end
