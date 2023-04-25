33308494a9b460e2d2f77ff5f6ea7b5ec4e959b2
Guilherme <walrusbane524@gmail.com>
2023-04-25 14:44:11 -0300

    Table view prototype

--- init.rb
+++ init.rb
@@ -5,11 +5,12 @@ Redmine::Plugin.register :tds do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
-  #permission :tds, { tds: [:index, :switch] }, public: true
   menu :project_menu, :tds, { controller: 'tds', action: 'index' }, caption: 'TDs', after: :activity, param: :project_id
   
   project_module :tds do
     permission :view_tds, tds: :index
+    permission :destroy_td, tds: :destroy
+    permission :switch_td, tds: :switch
     permission :create_tds, tds: :create
   end
 end
